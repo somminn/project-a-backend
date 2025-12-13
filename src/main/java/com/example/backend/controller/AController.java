6fad6cb75008ce18aa6f561a6e2a383662715e87
somminn <cowmin104@gmail.com>
2025-12-13 13:27:06 +0900

    [Feat] WebConfig 추가

--- src/main/java/com/example/backend/controller/AController.java
+++ src/main/java/com/example/backend/controller/AController.java
@@ -1,4 +1,4 @@
-package com.example.backend;
+package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AController {
     @GetMapping("/")
     public String check() {
-        return "Side Project A";
+        return "OK";
     }
 }
