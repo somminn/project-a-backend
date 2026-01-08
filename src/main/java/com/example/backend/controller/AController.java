11b41c9d531a64dfdcd30347897a0176b78bd085
somminn <cowmin104@gmail.com>
2026-01-08 14:48:49 +0900

    [Test] argo rollouts 테스트2

--- src/main/java/com/example/backend/controller/AController.java
+++ src/main/java/com/example/backend/controller/AController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class AController {
     @GetMapping("/")
     public String check() {
-        return "Test1";
+        return "Test2";
     }
 }
